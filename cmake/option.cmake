f9ce1cdf7c921ac4ec374244d56f5e7df543a9cc
HEYAHONG <2229388563@qq.com>
2021-09-05 14:48:52 +0800

    初步测试

---
+++ cmake/option.cmake
@@ -0,0 +1,39 @@
+
+
+set(CMAKE_C_FLAGS " ${CMAKE_C_FLAGS} \
+		-Wall \
+		-DGCC_COMPILE=1 \
+		-mthumb \
+		-Os \
+		--function-sections \
+		--data-sections \
+		-mcpu=cortex-m3 \
+		-std=gnu99 \
+		-march=armv7-m \
+		-mabi=aapcs \
+		-fno-builtin	\
+		-w ")
+set(CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} \
+                -Wall \
+                -DGCC_COMPILE=1 \
+                -mthumb \
+                -Os \
+                --function-sections \
+                --data-sections \
+                -mcpu=cortex-m3 \
+                -std=c++11 \
+                -march=armv7-m \
+                -mabi=aapcs \
+                -fno-builtin    \
+                -w " )
+
+
+set(CMAKE_EXE_LINKER_FLAGS " ${CMAKE_EXE_LINKER_FLAGS}\
+		-static	\
+		-nostartfiles	\
+		-mthumb	\
+		-mcpu=cortex-m3 ")
+
+
+set(BUILD_SHARED_LIBS OFF)
+
---
+++ cmake/toolchain.cmake
@@ -0,0 +1,26 @@
+
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ADDR2LINE arm-none-eabi-addr2line)
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_RANLIB arm-none-eabi-ranlib)
+set(CMAKE_CXX_COMPILER_AR arm-none-eabi-ar)
+set(CMAKE_CXX_COMPILER_RANLIB arm-none-eabi-ranlib)
+set(CMAKE_C_COMPILER_AR arm-none-eabi-ar)
+set(CMAKE_C_COMPILER_RANLIB arm-none-eabi-ranlib)
+set(CMAKE_Fortran_COMPILER arm-none-eabi-gfortan)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP arm-none-eabi-objdump)
+set(CMAKE_RC_COMPILER arm-none-eabi-windres)
+set(CMAKE_READELF arm-none-eabi-readelf)
+set(CMAKE_STRIP arm-none-eabi-strip)
+set(CMAKE_LINKER arm-none-eabi-ld)
+
+set(CMAKE_EXECUTABLE_SUFFIX_ASM .elf)
+set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
+set(CMAKE_EXECUTABLE_SUFFIX_CXX .elf)
+
+execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE gcc_version OUTPUT_STRIP_TRAILING_WHITESPACE)
+
